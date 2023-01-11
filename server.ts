7a925cbc76c05713ff80bf7e7ee595f60361fa91
personal <shaneel_kumar@live.com>
2023-01-11 14:22:41 +1300

    change order of endpoints

--- server.ts
+++ server.ts
@@ -14,13 +14,14 @@ app.use(express.urlencoded({ extended:false }))
 /* Routes */
 app.use('/filters', filtersRouter)
 
+app.get('/greet', (req, res) => {
+  return res.send('Hello')
+})
+
 app.get('/', (req, res) => {
   return res.send('Server is up & running.')
 })
 
-app.get('/greet', (req, res) => {
-  return res.send('Hello')
-})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
